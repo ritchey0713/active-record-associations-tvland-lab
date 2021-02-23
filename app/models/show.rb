c195bdaf1fa29cf6a625e72c8d48d839c466b48c
ritchey0713 <ritchey0713@gmail.com>
2021-02-23 12:31:42 -0500

    commit tree 8a3787eae61aa96b8e7b96c3d9fec277fc106c2c

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,7 @@
 class Show < ActiveRecord::Base
   has_many :characters
   belongs_to :network
-  has_many :actors, through: :characters
+  has_many :actor, through: :characters
 
   def actors_list
     self.actors.map{|actor| actor.full_name}
